480edb6d7db8389ce7391c55f0e61d78e067d842
Manoel Campos <manoelcampos@gmail.com>
2023-09-07 22:42:04 -0300

    Optimize imports
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/example/reactiverestapi/model/Produto.java
+++ src/main/java/com/example/reactiverestapi/model/Produto.java
@@ -5,12 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter @Setter
 @Entity
 public class Produto {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
