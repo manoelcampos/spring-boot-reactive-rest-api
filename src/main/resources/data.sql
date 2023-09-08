f533920515ecda4d1a6f8769ecd5afcf081984f0
Manoel Campos <manoelcampos@gmail.com>
2023-09-08 00:59:24 -0300

    Update data.sql
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into product (name) values ('TV');
 insert into product (name) values ('iPhone');
 insert into product (name) values ('Galaxy S22');
-insert into product (name) values ('Ventilador');
-insert into product (name) values ('Geladeira');
+insert into product (name) values ('Bed');
+insert into product (name) values ('Bike');
